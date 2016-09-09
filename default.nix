65ce3c46bdb58c020a1dac53e618d89c8b3b7dea
Márton Boros <martonboros@gmail.com>
2016-09-10 01:08:53 +0200

    wrap script env

--- default.nix
+++ default.nix
@@ -4,8 +4,12 @@ in rec {
   nixform = pkgs.stdenv.mkDerivation rec {
     name = "nixform";
     version = "0.1.0";
-    buildInputs = [ pkgs.terraform pkgs.yajl ];
-    installPhase = ''mkdir -p $out/bin && cp nixform $out/bin'';
+    buildInputs = [ pkgs.makeWrapper pkgs.terraform pkgs.yajl ];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp nixform $out/bin
+      wrapProgram $out/bin/nixform --suffix PATH : ${pkgs.terraform}/bin:${pkgs.yajl}/bin
+    '';
     src = ./.;
   };
 }
