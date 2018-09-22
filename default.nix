3f091c9df7dcc2bfae3ea94549aee67b605b4497
srghma <srghma@gmail.com>
2018-09-22 10:38:55 +0300

    fix: tests

--- default.nix
+++ default.nix
@@ -4,19 +4,17 @@ let
   inherit (lib) sourceByRegex;
 in
 
-rec {
-  nixform = stdenv.mkDerivation rec {
-    name = "nixform";
-    version = "0.1.0";
-    buildInputs = [ makeWrapper terraform yajl gnused ];
-    installPhase = ''
-      mkdir -p $out/bin
-      cp nixform $out/bin
-      wrapProgram $out/bin/nixform --suffix PATH : ${terraform}/bin:${yajl}/bin:${gnused}/bin
-    '';
+stdenv.mkDerivation rec {
+  name = "nixform";
+  version = "0.1.0";
+  buildInputs = [ makeWrapper terraform yajl gnused ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp nixform $out/bin
+    wrapProgram $out/bin/nixform --suffix PATH : ${terraform}/bin:${yajl}/bin:${gnused}/bin
+  '';
 
-    src = sourceByRegex ./. [
-      "^nixform$"
-    ];
-  };
+  src = sourceByRegex ./. [
+    "^nixform$"
+  ];
 }
